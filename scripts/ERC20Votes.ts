cbf63d0642393a57dc9c4760673378f29feb281d
JK1331 <jakubkubala3@gmail.com>
2022-10-07 19:12:35 +0200

    Working on ERC20Votes script

--- scripts/ERC20Votes.ts
+++ scripts/ERC20Votes.ts
@@ -1,9 +1,14 @@
-import { ethers } from "ethers";
+import { ethers } from "hardhat";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-
+    const myErc20VoteFactory = await ethers.getContractFactory("MyERC20Vote");
+    const myErc20Vote = await myErc20VoteFactory.deploy();
+    await myErc20Vote.deployed();
+    console.log(`My token contract was deployed at the address of ${myErc20Vote.address}`);
+    const totalSupply = await myErc20Vote.totalSupply();
+    console.log(`The initial total supply of this contract after deployment is ${totalSupply}`);
 }
 
 main().catch((error) => {
