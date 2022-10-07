387d8f2e164145ba94972078594ac265d4e3d279
JK1331 <jakubkubala3@gmail.com>
2022-10-07 20:41:49 +0200

    Adding state variables and contructor to TokenizedBallot

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -1,2 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+interface ITokenizedVotes {
+
+}
+
+contract TokenizedBallot {
+    uint256 public referenceBlock;
+    ITokenizedVotes public tokenContract;
+
+
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    }
+
+    Proposal[] public proposals;
+
+    constructor(bytes32[] memory proposalNames, address _voteTokenContract, uint256 _referenceBlock) {
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({ name: proposalNames[i], voteCount: 0 }));
+        }
+        tokenContract = ITokenizedVotes(_voteTokenContract);
+        referenceBlock = _referenceBlock;
+    }
+}
