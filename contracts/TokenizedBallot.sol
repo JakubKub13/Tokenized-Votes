c71f50421f91db903785b7c6905611bcf5acb4ff
JK1331 <jakubkubala3@gmail.com>
2022-10-07 20:48:54 +0200

    Adding view functions TokenizedBallot

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -24,4 +24,18 @@ contract TokenizedBallot {
         tokenContract = ITokenizedVotes(_voteTokenContract);
         referenceBlock = _referenceBlock;
     }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
 }
