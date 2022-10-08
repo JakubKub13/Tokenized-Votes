08477a8b224a7e175bffc63fc805d41135434559
JK1331 <jakubkubala3@gmail.com>
2022-10-08 08:28:52 +0200

    Adding deployToken script

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -4,7 +4,11 @@ dotenv.config();
 
 
 async function main() {
-  
+    const [deployer] = await ethers.getSigners();
+    const myErc20VoteFactory = await ethers.getContractFactory("MyERC20Vote");
+    const myErc20Vote = await myErc20VoteFactory.deploy();
+    await myErc20Vote.deployed();
+    console.log(`My token contract was deployed at the address of ${myErc20Vote.address}`);
 }
 
 
