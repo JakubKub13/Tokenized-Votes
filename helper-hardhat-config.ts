c72acb52285e4d03c144d41eccb925ccbcaa6c7b
JK1331 <jakubkubala3@gmail.com>
2022-10-08 17:44:03 +0200

    Code refactoring

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -8,6 +8,7 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
     31337: {
         name: "localhost",
+        myERC20Vote: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     },
 
     5: {
--- scripts/deployBallot.ts
+++ scripts/deployBallot.ts
@@ -1,15 +1,29 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import * as dotenv from "dotenv";
+import { networkConfig } from "../helper-hardhat-config";
 dotenv.config();
 
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 
 async function main() {
+  function convertStringArrayToBytes32(array: string[]) {
+    const bytes32Array = [];
+    for (let index = 0; index < array.length; index++) {
+      bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+    }
+    return bytes32Array;
+  }
   
+  const [deployer] = await ethers.getSigners();
+  const tokenizedBallotFactory = await ethers.getContractFactory("TokenizedBallot");
+  const tokenizedBallot = await tokenizedBallotFactory.deploy(convertStringArrayToBytes32(PROPOSALS), "0x5FbDB2315678afecb367f032d93F642f64180aa3", 0);
+  await tokenizedBallot.deployed();
+  console.log(`Tokenized Ballot contract was deployed ad : ${tokenizedBallot.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
