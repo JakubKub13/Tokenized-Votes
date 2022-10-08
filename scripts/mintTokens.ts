00820adce0361d8cbe651622897293bb7813b16a
JK1331 <jakubkubala3@gmail.com>
2022-10-08 18:02:37 +0200

    Adding minting script

--- scripts/mintTokens.ts
+++ scripts/mintTokens.ts
@@ -1,10 +1,32 @@
 import { ethers } from "hardhat";
 import * as dotenv from "dotenv";
+import { MyERC20Vote } from "../typechain-types";
 dotenv.config();
 
 
 async function main() {
-  
+    let myErc20Vote: MyERC20Vote;
+    const MINTING_AMOUNT = 1;
+
+    const [deployer, account1, account2] = await ethers.getSigners();
+    myErc20Vote = await ethers.getContractAt("MyERC20Vote", "0x5FbDB2315678afecb367f032d93F642f64180aa3", deployer);
+    let deployerInitBalance = await myErc20Vote.balanceOf(deployer.address);
+    let deployerInitBalanceFormatted = deployerInitBalance.toNumber();
+    console.log(`Initial balance of deployer address is : ${deployerInitBalanceFormatted}`);
+    
+    await myErc20Vote.mint(deployer.address, MINTING_AMOUNT);
+    let deployerAfterMintBalance = await myErc20Vote.balanceOf(deployer.address);
+    let deployerAfterMintBalFormatted = deployerAfterMintBalance.toNumber();
+    console.log(`Balance of deployer after first mint is : ${deployerAfterMintBalFormatted}`);
+
+    let acc1InitBalance = await myErc20Vote.balanceOf(account1.address);
+    let acc1InitBalFormatted = acc1InitBalance.toNumber();
+    console.log(`Initial balance of account 1 is : ${acc1InitBalFormatted}`);
+
+    await myErc20Vote.mint(account1.address, MINTING_AMOUNT);
+    let acc1BalAfterMint = await myErc20Vote.balanceOf(account1.address);
+    let acc1BalAfterMintFormatted = acc1BalAfterMint.toNumber();
+    console.log(`Balance of account 1 after first minting is: ${acc1BalAfterMintFormatted}`);
 }
 
 
