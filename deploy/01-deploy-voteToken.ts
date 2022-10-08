3ab192718ced261352873ab9f0f28c732e90a992
JK1331 <jakubkubala3@gmail.com>
2022-10-08 08:45:59 +0200

    Adding helper-hardhat-config script

--- deploy/01-deploy-voteToken.ts
+++ deploy/01-deploy-voteToken.ts
@@ -1,7 +1,7 @@
 import { getNamedAccounts, deployments, network, run } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { NetworkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config";
+import { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config";
 import verify from "../verify";
 
 const deployINFTtoken: DeployFunction = async function (
