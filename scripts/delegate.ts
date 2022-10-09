cdc2e389ad89034dbbf6e171e7210a3656b0f843
JK1331 <jakubkubala3@gmail.com>
2022-10-09 08:27:09 +0200

    Code refactoring

--- scripts/delegate.ts
+++ scripts/delegate.ts
@@ -1,13 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import * as dotenv from "dotenv";
 import { MyERC20Vote } from "../typechain-types";
 dotenv.config();
+import { developmentChains, networkConfig } from "../helper-hardhat-config";
 
 async function main() {
     let myErc20Vote: MyERC20Vote;
+    let account1: SignerWithAddress
 
-    const [deployer, account1] = await ethers.getSigners();
-    myErc20Vote = await ethers.getContractAt("MyERC20Vote", "0x5FbDB2315678afecb367f032d93F642f64180aa3", deployer);
+    const [deployer] = await ethers.getSigners();
+    account1 = await ethers.getSigner("0x01825FD823d3Bc1806115B011980068bE6405C11")
+    const chainId = network.config.chainId;
+    const tokenAddr = networkConfig[chainId]["myERC20Vote"];
+
+    myErc20Vote = await ethers.getContractAt("MyERC20Vote", tokenAddr, deployer);
     let deployerInitVotes = await myErc20Vote.getVotes(deployer.address);
     let deployerInitVotesFormatted = deployerInitVotes.toString();
     console.log(`Deployer initial votes before delegation are: ${deployerInitVotesFormatted}`);
